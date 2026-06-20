c83a6fa7f9be9a38c1213f60e727f4769d2f6a67
Maltomatic <jjbugman2468@gmail.com>
2026-06-20 12:00:13 -0700

    initial POC for plant reading

---
+++ POC_electrode_reader/POC_electrode_reader.ino
@@ -0,0 +1,27 @@
+#include "esp_system.h"
+#include <Arduino.h>
+#include <math.h>
+
+const int PIN_PLANT = 11;   // change if you used a different ADC pin
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial){
+    ;
+  }
+  analogReadResolution(12);     // ESP32-S3: 0–4095
+  analogSetAttenuation(ADC_11db); // good for 0–3.3V range
+  Serial.println("Start");
+}
+
+void loop() {
+  int raw = analogRead(PIN_PLANT);   // 0–4095
+  float volts = raw * (3.3f / 4095.0f);
+
+  Serial.print("RAW: ");
+  Serial.print(raw);
+  Serial.print("   VOLTS: ");
+  Serial.println(volts, 4);
+
+  delay(200);  // ~50 Hz sampling
+}
